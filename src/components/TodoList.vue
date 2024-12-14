3fc66bebccc589eacaad8ea42285aade7accbb05
unknown <santoyoisabella14@gmail.com>
2024-12-14 18:13:30 -0400

    primera version de la tarea

---
+++ src/components/TodoList.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+    import ListItem from './ListItem.vue';
+</script>
+
+<template>
+    <ul>
+        <ListItem :isChecked="false"> Esta es la tarea 
+        </ListItem>
+    </ul>
+</template>
+
+<style>
+</style>
